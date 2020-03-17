10fb5b8e49f44383cb137732e93b3143d791fd35
syafiq <mr.syafiqrafi95@gmail.com>
2020-03-17 11:04:14 +0800

    asdasdsadasdasdad asdasdasd update for coding test
    Collecting employee information for a company (There are N people )

---
+++ codingtest.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+class People  //  abstract base class
+{
+    virtual float getSalary() = 0;  // pure virtual function
+};
+class Executives: public People
+{
+    int salary;
+    public:
+    Executives(int s)
+    {
+        salary = s;
+    }
+    float getSalary()
+    {
+        return salary;
+    }
+};
+class Software_engineer: public People
+{
+    float salary;
+    public:
+    Software_engineer(int t)
+    {
+        salary = t;
+    }
+    float getSalary()
+    {
+        return salary;
+    }
+};
+class Hardware_engineer: public People
+{
+    float salary;
+    public:
+    Hardware_engineer(int t)
+    {
+        salary = t;
+    }
+    float getSalary()
+    {
+        return salary;
+    }
+};
+int main()
+{
+    Executives e(5000);
+    Software_engineer s_e(30000);
+    Hardware_engineer h_e(5000);
+    float a,b,c;
+    a = e.getSalary();
+    b = s_e.getSalary();
+    c = h_e.getSalary();
+    cout << "Salary of Executives : " << a << endl;
+    cout << "Salary of Software Engineers : " << b << endl;
+    cout << "Salary of Hardware Engineers : " << c << endl;
+    return 0;
+}
