829f848f421c6498ebd47e72a02277eb958c6561
luqmanul <luqmanulyazib@gmail.com>
2020-03-17 01:15:25 -0400

    Makefile Macros option
    Collecting employee information for a company (There are N people )
    
    makefile macros
    
    Collecting employee information for a company (There are N people )

---
+++ homework/makefiletest/makefiletest-3/lib/testlib.cpp
@@ -0,0 +1,6 @@
+#include"testlib.h"
+
+void showtest(void)
+{
+	printf("this is a lib test test \n");
+}
---
+++ homework/makefiletest/makefiletest-3/main.cpp
@@ -0,0 +1,74 @@
+#ifdef USE_MYTEST
+ #include"testlib.h"
+#endif
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+class people{
+
+    protected:
+        int workinghour;
+     public:
+        void setworkinghour(int a){
+        workinghour=a;
+        }
+
+
+};
+
+class executive: public people{
+   public:
+      void hour()
+      {
+       cout << " im an executive , my working hour is " <<workinghour<<endl;
+      }
+};
+
+class software_engineer:public people{
+  public:
+    virtual void hour()
+    {
+    cout << " im a software engineer,my working hour is " <<workinghour<<endl;
+   }
+};
+
+class hardware_engineer:public people{
+   public:
+   void hour()
+     {
+      cout << " im a hardware engineer,my working hour is " <<workinghour<<endl;
+     }
+};
+
+int main()
+{
+
+ executive e;
+ software_engineer se;
+ hardware_engineer he;
+ people *employees1 = &e;
+ people *employees2 =&se;
+ people *employees3 =&he;
+ employees1 ->setworkinghour(8);
+ employees2 ->setworkinghour(12);
+ employees3 ->setworkinghour(8);
+ e.hour();
+ se.hour();
+ he.hour();
+#ifdef USE_MYTEST
+ showtest();
+#endif
+
+}
+
+
+
+
+
+
+
+
+
+
+
